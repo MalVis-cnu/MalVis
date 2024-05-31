b2e80b8145fac6a2671b7ed08a8cd8d087593002
helloooow <201902669@o.cnu.ac.kr>
2024-05-31 18:46:21 +0900

    fix result option

--- model/main.py
+++ model/main.py
@@ -81,12 +81,12 @@ def main(args):
     clustering_result['hash'] = hash
     clustering_result['sequence_data'] = input_data
     clustering_result['similar_sequence_matrix'] = similar_sequence_matrix
-    clustering_result['option'] = json.dumps({
+    clustering_result['option'] = {
         'similarity_method' : args.similarity_method,
-        'similarity_option' : args.similarity_option,
+        'similarity_option' : option_parsing(args.similarity_option),
         'clustering_method' : args.clustering_method,
-        'clustering_option' : args.clustering_option
-    })
+        'clustering_option' : option_parsing(args.clustering_option)
+    }
 
     
     # for i in clustering_result.items(): print(i)
